861cb08ca7923f149bd907a9902fbeaff5cad61a
Pietro Basso <bassop94dev@gmail.com>
2019-03-21 10:28:37 +0100

    Update Item Matcher problem.

--- Practice/Interview Preparation Kit/Item Matcher/ItemMatcher.playground/Contents.swift
+++ Practice/Interview Preparation Kit/Item Matcher/ItemMatcher.playground/Contents.swift
@@ -7,6 +7,14 @@ protocol Item {
 }
 
 extension Item {
+    func narrowMatches(group: String) -> Bool {
+        return self.group == group
+    }
+    
+    func narrowMatches(category: String) -> Bool {
+        return self.category == category
+    }
+    
     func broadMatches(group: String) -> Bool {
         return self.group == group || self.group == "*"
     }
@@ -25,11 +33,29 @@ class ItemMatcher {
     
     // Complete the itemMatching function below.
     func itemMatching(group: String, category: String) -> Item? {
-        let matchingItem = items.first { $0.group == group && $0.category == category }
-        guard matchingItem != nil else {
-            return items.first { $0.broadMatches(group: group) && $0.broadMatches(category: category) }
-        }
-        return matchingItem
+        var result: Item? = nil
+        result = items.first { $0.narrowMatches(group: group) && $0.narrowMatches(category: category) }
+        if result != nil { return result }
+        result = items.first { $0.broadMatches(group: group) && $0.narrowMatches(category: category) }
+        if result != nil { return result }
+        result = items.first { $0.narrowMatches(group: group) && $0.broadMatches(category: category) }
+        if result != nil { return result }
+        return items.first { $0.broadMatches(group: group) && $0.broadMatches(category: category) }
     }
     
 }
+
+struct ItemImplementation: Item {
+    let category: String
+    let group: String
+}
+
+let matcher = ItemMatcher(items: [ItemImplementation(category: "*", group: "*"),
+                                  ItemImplementation(category: "b1", group: "b2"),
+                                  ItemImplementation(category: "c1", group: "*"),
+                                  ItemImplementation(category: "d1", group: "d2"),
+                                  ItemImplementation(category: "e1", group: "e2"),
+                                  ItemImplementation(category: "f1", group: "f2"),
+                                  ItemImplementation(category: "g1", group: "g2")])
+
+matcher.itemMatching(group: "e2", category: "c1")
